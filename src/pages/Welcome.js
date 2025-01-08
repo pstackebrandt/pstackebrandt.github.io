cf5d9e25687c25d16a8c0e05b3d4f369c0a1a80a
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-01-08 16:28:41 +0100

    Update dependencies and refactor Welcome.js for improved styling and structure (in the future)
    - Added 'sass' as a new devDependency in package.json and package-lock.json for enhanced styling capabilities.
    - Refactored Welcome.js to improve the layout and styling, introducing a container for better structure and updated class names for clarity.
    - Enhanced the welcome message with improved text formatting and organization, making it more visually appealing and easier to read.
    - Updated the project to ensure compatibility with the latest dependencies, contributing to overall stability and performance.

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,23 +1,24 @@
 import React from "react"
-
 import PageTitle from '../pages/page-title';
+import '../styles/components/welcome.scss';
 
 // Main content of the page
 export default function Welcome() {
-
     return (
-        <div className="welcome ms-5 me-5">
-            <PageTitle />
-            <h1 className="mt-5 m-5 text-center">Willkommen</h1>
-            <p className="text-center mb-2 fs-5">Ich bin Peter Stackebrandt, Softwareentwickler aus Feucht bei Nürnberg mit langjähriger Erfahrung in der Entwicklung von Apps und Services mit .NET. Neben meiner Leidenschaft für Webentwicklung interessiere ich mich besonders für den Einsatz von KI in der Softwareentwicklung.</p>
-            <p className="text-center mb-2 fs-5">Diese Webseite ist eine zentrale Anlaufstelle, um mehr über meine Arbeit und meine Projekte zu erfahren. Derzeit arbeite ich daran, öffentlich zugängliche Links zu meinen Projekten hier zu sammeln und zu präsentieren.</p>
-            <p className="text-center mb-2 fs-5">Bereits verfügbare Links:</p>
-            <ul className="text-center mb-2 fs-5">
-                <li><a href="https://github.com/pstackebrandt">Mein GitHub Repository</a>: Enthält den Code vieler Übungsprojekte.</li>
-                <li><a href="https://www.linkedin.com/in/">Mein LinkedIn-Profil</a>:Ausführliche Informationen zu meinem beruflichen Profil</li>
-                <li><a href="https://www.bilderbrand.net/">Bilderbrand.net</a>: Eine private Homepage, die ich mit WordPress erstellt habe.</li>
-            </ul>
-            <p className="text-center mb-2 fs-5">Diese Webseite dient zusätzlich als persönliches Projekt, um moderne Webtechnologien wie React zu erlernen und anzuwenden.</p>
+        <div className="welcome">
+            <div className="welcome__container">
+                <PageTitle />
+                <h1 className="welcome__title">Willkommen</h1>
+                <p className="welcome__text">Ich bin Peter Stackebrandt, Softwareentwickler aus Feucht bei Nürnberg mit langjähriger Erfahrung in der Entwicklung von Apps und Services mit .NET. Neben meiner Leidenschaft für Webentwicklung interessiere ich mich besonders für den Einsatz von KI in der Softwareentwicklung.</p>
+                <p className="welcome__text">Diese Webseite ist eine zentrale Anlaufstelle, um mehr über meine Arbeit und meine Projekte zu erfahren. Derzeit arbeite ich daran, öffentlich zugängliche Links zu meinen Projekten hier zu sammeln und zu präsentieren.</p>
+                <p className="welcome__text">Bereits verfügbare Links:</p>
+                <ul className="welcome__list">
+                    <li><a className="welcome__link" href="https://github.com/pstackebrandt">Mein GitHub Repository</a>: Enthält den Code vieler Übungsprojekte.</li>
+                    <li><a className="welcome__link" href="https://www.linkedin.com/in/">Mein LinkedIn-Profil</a>: Ausführliche Informationen zu meinem beruflichen Profil</li>
+                    <li><a className="welcome__link" href="https://www.bilderbrand.net/">Bilderbrand.net</a>: Eine private Homepage, die ich mit WordPress erstellt habe.</li>
+                </ul>
+                <p className="welcome__text">Diese Webseite dient zusätzlich als persönliches Projekt, um moderne Webtechnologien wie React zu erlernen und anzuwenden.</p>
+            </div>
         </div>
     );
 }
