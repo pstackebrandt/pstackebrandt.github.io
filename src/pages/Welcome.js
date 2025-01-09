7ec0cc3404fdb30bb991d5fc298c30309d8376d6
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-01-09 16:03:09 +0100

    Add link to gas-costs-app Separate LinkedIn from projects section
    Refactor Welcome.js and welcome.scss for enhanced content presentation and styling
    
    - Revised the personal introduction in Welcome.js to improve clarity and emphasize the developer's background and interests.
    - Updated project links with better formatting and accessibility, including new external link attributes.
    - Enhanced welcome.scss with new styles for improved layout, including a new section title class and refined link hover effects.
    - Added a new project link for the Fahrtkosten-Rechner-App, providing a brief description of its functionality.
    - Improved overall responsiveness and user experience across the welcome component.

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -9,11 +9,25 @@ export default function Welcome() {
             <div className="welcome__container">
                 <PageTitle />
 
-                <p className="welcome__text">Ich wohne in Feucht bei Nürnberg und habe langjährige Erfahrung in der Entwicklung von Apps und Services mit .NET.
-                    Neben meiner Leidenschaft für Webentwicklung interessiere ich mich besonders für den Einsatz von KI in der Softwareentwicklung.</p>
-                <p className="welcome__text">Diese Webseite ist eine zentrale Anlaufstelle, um mehr über meine Arbeit und meine Projekte zu erfahren. Derzeit arbeite ich daran, öffentlich zugängliche Links zu meinen Projekten hier zu sammeln und zu präsentieren.</p>
-                <p className="welcome__subtitle">Links</p>
+                <p className="welcome__text">Ich wohne in Feucht bei Nürnberg und habe langjährige Erfahrung in der Entwicklung von Apps und Services mit .NET. Neben meiner Leidenschaft für Webentwicklung interessiere ich mich besonders für den Einsatz von KI in der Softwareentwicklung.</p>
+
+                <p className="welcome__text">Diese Webseite ist eine zentrale Anlaufstelle, um mehr über meine Fertigkeiten und meine Projekte zu erfahren. Derzeit arbeite ich daran, hier öffentlich zugängliche Links zu meinen Projekten zu sammeln und zu präsentieren.</p>
+
+                <a className="welcome__link" href="https://www.linkedin.com/in/" target="_blank" rel="noopener noreferrer">
+                    Mein LinkedIn-Profil
+                </a>
+                <span>Ausführliche Informationen zu meinem beruflichen Profil</span>
+
+                <p className="welcome__subtitle">Projekte</p>
                 <ul className="welcome__list">
+                    <li>
+                        <a className="welcome__link" href="https://pstackebrandt.github.io/gas-costs-app/" target="_blank" rel="noopener noreferrer">
+                            Fahrtkosten-Rechner-App
+                        </a>
+                        <span>Berechnet die Kosten für eine Fahrt anhand gefahrener Kilometer, des Kraftstoffverbrauchs und des Kraftstoffpreises.
+                            (React App, nicht responsiv)
+                        </span>
+                    </li>
                     <li>
                         <a className="welcome__link" href="https://github.com/pstackebrandt" target="_blank" rel="noopener noreferrer">
                             Mein GitHub Repository
@@ -21,19 +35,18 @@ export default function Welcome() {
                         <span>Enthält den Code vieler Übungsprojekte.</span>
                     </li>
                     <li>
-                        <a className="welcome__link" href="https://www.linkedin.com/in/" target="_blank" rel="noopener noreferrer">
-                            Mein LinkedIn-Profil
-                        </a>
-                        <span>Ausführliche Informationen zu meinem beruflichen Profil</span>
-                    </li>
-                    <li>
                         <a className="welcome__link" href="https://www.bilderbrand.net/" target="_blank" rel="noopener noreferrer">
                             Bilderbrand.net
                         </a>
-                        <span>Eine private Homepage, die ich mit WordPress erstellt habe.</span>
+                        <span>Meine private Homepage, die ich mit WordPress erstellt habe.</span>
+                    </li>
+                    <li>
+                        <a className="welcome__link" href="/" target="_blank" rel="noopener noreferrer">
+                            Die Webseite,
+                        </a>
+                        <span>die sie gerade besuchen, ist ebenfalls ein Trainingsprojekt. Sie ist responsiv und wurde mit React, React Router und SCSS geschrieben.</span>
                     </li>
                 </ul>
-                <p className="welcome__text">Diese Webseite dient zusätzlich als persönliches Projekt, um moderne Webtechnologien wie React zu erlernen und anzuwenden.</p>
             </div>
         </div>
     );
