fc7fc34ec00f44b22c513fc82fffbccfe35bc2a8
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-01-13 11:28:16 +0100

    Update index title, remove SingleLinkWithDescription component, and add color variables to _variables.scss
    - Changed the document title in index.html from "React App" to "Peter Stackebrandt" for better branding.
    - Deleted the SingleLinkWithDescription component as it was no longer needed.
    - Introduced a comprehensive set of color variables in _variables.scss to enhance styling consistency and maintainability across the project.

--- src/components/SingleLinkWithDescription.js
+++
@@ -1,8 +0,0 @@
-const SingleLinkWithDescription = ({ url, linkText, description }) => (
-  <div className="single-link-with-description">
-    <a href={url} className="single-link">{linkText}</a>
-    <span className="single-link-description">{description}</span>
-  </div>
-);
-
-export default SingleLinkWithDescription; 
